8153efc018238511b65d8954d00850b92a0eeb1e
dsukmanova <sukmanovadarya@yandex..ru>
2022-04-22 15:18:16 +0300

    new tasks

--- src/main/java/D.java
+++ src/main/java/D.java
@@ -1,4 +1,53 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
+
+//если школа стоит между городами то суммарное расстояние либо нарастает либо убывает  -> можно смотреть в точках где школы
 
 public class D {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] data = new int[n];
+        long curSum = 0L;
+        long minSum = 0L;
+        int minPoint;
+        int k = 0; //сколько домов находятся строго до текущей позиции
+        int first = sc.nextInt();
+        data[0] = first;
+
+        for (int i = 1; i < n; i++) {
+            data[i] = sc.nextInt();
+            curSum += (data[i] - first);
+        }
+
+        minSum = curSum;
+        minPoint = first;
+        k = 1;
+        /*for (int i = first + 1; i <= data[n - 1]; i++) {
+            curSum += (2 * k - n);
+            if (curSum < minSum) {
+                minSum = curSum;
+                minPoint = i;
+            }
+            if (data[k] == i) {
+                k++;
+            }
+
+        }*/
+        //1 2 5
+        //5
+        //5 + (2 -3)*1=4
+        //4 + (4-3) * 3=7
+
+        for (int j = 1; j < n; j++) {
+            curSum += (2*k - n) * (data[j] - data[j-1]);
+            if (curSum < minSum) {
+                minSum = curSum;
+                minPoint = data[j];
+            }
+            k++;
+        }
+
+        System.out.println(minPoint);
+    }
+
 }
--- src/main/java/E.java
+++ src/main/java/E.java
@@ -1,4 +1,35 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class E {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int d = sc.nextInt();
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+
+        int res;
+        if (x <= 0) {
+            if (2 * y <= d) {
+                res = 1;
+            } else {
+                res = 3;
+            }
+            if (x == 0 && y >= 0 && y <= d) {
+                res = 0;
+            }
+        } else {
+            if (y < 0) {
+                res = (2 * x <= d) ? 1 : 2;
+            } else if (y == 0) {
+                res = (x <= d) ? 0 : 2;
+            } else {
+                if (y <= -x + d) {
+                    res = 0;
+                } else {
+                    res = (x - d) * (x - d) <= (y - d) * (y - d) ? 2 : 3;
+                }
+            }
+        }
+        System.out.println(res);
+    }
 }
